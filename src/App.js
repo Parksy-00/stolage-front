f0870af05e9f0bc8ac45ab4380daf79e9abf12e0
Parksy-00 <63638539+Parksy-00@users.noreply.github.com>
2021-03-17 15:17:43 +0900

    login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import "antd/dist/antd.css"
+import React from 'react'
+import { Layout } from 'antd'
+
 import UploadPage from './components/uploadSpace/uploadSpace'
+import LoginSpace from './components/loginSpace/login'
+import MainSpace from './components/mainSpace/mainSpace'
+
+const { Sider, Content } = Layout
 
 function App() {
   return (
-    <div className="App" style={{display:"flex", alignItems:"center",justifyContent:"center", height:"100vh", width:"100%"}}>
-      <UploadPage/>
+    <div className="App" style={{height:"100vh", width:"100%"}}>
+      <Layout>
+        <Sider theme='light' width="400px" style={{minHeight:"100vh"}}>
+          <LoginSpace></LoginSpace>
+        </Sider>
+        <Content>
+          <MainSpace/>
+        </Content>
+      </Layout>
+
     </div>
   );
 }
---
+++ src/components/mainSpace/mainSpace.js
@@ -0,0 +1,21 @@
+import "antd/dist/antd.css"
+import { Typography} from 'antd'
+import "./mainSpace.css"
+
+const { Title } = Typography
+export default function MainSpace() {
+
+    return (
+        <div style={{height:"100%"}}>
+            <div style={{position:"relative", top:"30%", textAlign:'left', left:"30%"}}>
+                <Title className="title" style={{fontSize:"60px"}}>stolage</Title>
+                <p className="description" style={{ position:"absolute", top:"45px", display:"inline-block", fontSize:"middle"}}>Storage에 label을 입히다</p>
+                <p className="description" style={{fontSize:"x-large"}}>
+                    stolage는 파일을 단순한 디렉토리로 저장하지않습니다. <br/>
+                    파일을 label로 묘사하세요.
+                </p>
+            </div>
+            
+        </div>
+    )
+}
