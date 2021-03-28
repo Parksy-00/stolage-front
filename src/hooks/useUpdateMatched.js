6bfb6cea91046e8cc6abea539fe38d94b408f8df
yujong-lee <yujonglee@g.skku.edu>
2021-03-29 08:27:28 +0900

    refactor: replace callback with async/await

--- src/hooks/useUpdateMatched.js
+++ src/hooks/useUpdateMatched.js
@@ -3,15 +3,14 @@ import { useSetRecoilState } from "recoil"
 import Axios from 'axios'
 import matchedFiles from '../states/matchedFiles'
 
-const useUpdateMatched = (newSelected) => {
+const useUpdateMatched = newSelected => {
     const setFiles = useSetRecoilState(matchedFiles)
 
-    useEffect(() => {
+    useEffect(async () => {
         const body = { selected: newSelected }
-        Axios.post('http://localhost:5000/demo/search', body)
-            .then(res => {
-                setFiles(res.data)
-            })
+        const response = await Axios.post('http://localhost:5000/demo/search', body)
+
+        setFiles(response.data)    
     }, [newSelected])
 }
 
